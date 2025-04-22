944e6d911d0255c5b915935d41c056f28c848c07
thant-san <thanthtoosan.mechatronic@gmail.com>
2025-04-22 19:22:45 +0700

    add comments

--- src/animalshleter/InvalidAnimalTypeException.java
+++ src/animalshleter/InvalidAnimalTypeException.java
@@ -1,5 +1,6 @@
 package animalshelter;
 
+// Custom exception for handling invalid animal types
 public class InvalidAnimalTypeException extends Exception {
     public InvalidAnimalTypeException(String message) {
         super(message);
